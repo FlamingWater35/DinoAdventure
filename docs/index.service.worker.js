9d7911b3757cc1b34fb2923eb36c5daba786de63
unknown <flamingwater69420@gmail.com>
2024-10-14 10:43:24 +0300

    Update resize code

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728890720|778127684';
+const CACHE_VERSION = '1728891760|302879352';
 /** @type {string} */
 const CACHE_PREFIX = 'Dino Adventure-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
