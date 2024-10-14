83ce4b8ea72255164ae4b90bedb93a781f4a8ee4
unknown <flamingwater69420@gmail.com>
2024-10-14 10:58:11 +0300

    Removed printing label scale every delta time

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728891760|302879352';
+const CACHE_VERSION = '1728892583|138112203';
 /** @type {string} */
 const CACHE_PREFIX = 'Dino Adventure-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
